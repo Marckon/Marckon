ebdb872007e9a374ee5ce9fc7ff57e57d8035d39
修荼 <chengzhe.lcz@alibaba-inc.com>
2019-11-17 19:05:08 +0800

    Updates

---
+++ precache-manifest.5338f5d38ba5d28ea1949a4bd836e216.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "692451e1f8dd9f0bdbc4098819695a41",
+    "url": "/Marckon/index.html"
+  },
+  {
+    "revision": "5cc657647d853e142944",
+    "url": "/Marckon/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "418d64695df0fe919c18",
+    "url": "/Marckon/static/js/2.871f3899.chunk.js"
+  },
+  {
+    "revision": "5cc657647d853e142944",
+    "url": "/Marckon/static/js/main.df95e0f7.chunk.js"
+  },
+  {
+    "revision": "17369893bd6fc88bd01f",
+    "url": "/Marckon/static/js/runtime-main.43e54f30.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Marckon/static/media/logo.25bf045c.svg"
+  }
+]);
